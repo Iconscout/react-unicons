bdca9a89cc899e424f969ee29cd9c3be86408265
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-04-07 14:27:40 +0530

    Updated Unicons

---
+++ icons/uil-create-dashboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UilCreateDashboard = (props) => {
+  const { color, size, ...otherProps } = props
+  return React.createElement('svg', {
+    xmlns: 'http://www.w3.org/2000/svg',
+    width: size,
+    height: size,
+    viewBox: '0 0 24 24',
+    fill: color,
+    ...otherProps
+  }, React.createElement('path', {
+    d: 'M10,13H4a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V14A1,1,0,0,0,10,13ZM9,19H5V15H9ZM20,3H14a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V4A1,1,0,0,0,20,3ZM19,9H15V5h4Zm1,7H18V14a1,1,0,0,0-2,0v2H14a1,1,0,0,0,0,2h2v2a1,1,0,0,0,2,0V18h2a1,1,0,0,0,0-2ZM10,3H4A1,1,0,0,0,3,4v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V4A1,1,0,0,0,10,3ZM9,9H5V5H9Z'
+  }));
+};
+
+UilCreateDashboard.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+UilCreateDashboard.defaultProps = {
+  color: 'currentColor',
+  size: '24',
+};
+
+export default UilCreateDashboard;
