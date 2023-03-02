5d28e7fa4e746716bb90c6c4dc386901d1142e05
Arpan Rank <rankarpan@users.noreply.github.com>
2023-03-02 18:22:21 +0530

    Release 2.0.0 (#80)
    * Update from https://github.com/Iconscout/unicons/commit/762cff95e84f69b38a69520658696c433b610b37 for package
    
    * fix: unicons v4 & update license
    
    * [create-pull-request] automated change (#81)
    
    ---------
    
    Co-authored-by: tarunmangukiya <tarunmangukiya@hotmail.com>

--- icons/uil-microsoft.js
+++ icons/uil-microsoft.js
@@ -11,7 +11,7 @@ const UilMicrosoft = (props) => {
     fill: color,
     ...otherProps
   }, React.createElement('path', {
-    d: 'M2,22h9.5v-9.5H2V22z M2,11.5h9.5V2H2V11.5z M12.5,2v9.5H22V2H12.5z M12.5,22H22v-9.5h-9.5V22z'
+    d: 'M2 22h9.5v-9.5H2V22zm0-10.5h9.5V2H2v9.5zM12.5 2v9.5H22V2h-9.5zm0 20H22v-9.5h-9.5V22z'
   }));
 };
 
