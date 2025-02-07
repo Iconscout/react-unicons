17a890e85b7aab19f39162b40cafc3810fa21303
Arpan Rank <rankarpan@users.noreply.github.com>
2025-02-07 15:45:54 +0530

    Release 2.2.4 (#122)
    * hotfix: remove exports key that was breaking some imports (#120)
    
    * Fix: remove exports key that was breaking some imports
    
    * Fix: last version was already there, updating the new
    
    * [create-pull-request] automated change (#121)
    
    Co-authored-by: rankarpan <9264092+rankarpan@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Zaid Kureshi <48899828+imzedi@users.noreply.github.com>
    Co-authored-by: rankarpan <9264092+rankarpan@users.noreply.github.com>

--- icons/uil-stroller.js
+++ icons/uil-stroller.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const UilBabyCarriage = (props) => {
+const UilStroller = (props) => {
   const { color, size, ...otherProps } = props
   return React.createElement('svg', {
     xmlns: 'http://www.w3.org/2000/svg',
@@ -15,14 +15,14 @@ const UilBabyCarriage = (props) => {
   }));
 };
 
-UilBabyCarriage.propTypes = {
+UilStroller.propTypes = {
   color: PropTypes.string,
   size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
-UilBabyCarriage.defaultProps = {
+UilStroller.defaultProps = {
   color: 'currentColor',
   size: '24',
 };
 
-export default UilBabyCarriage;
+export default UilStroller;
